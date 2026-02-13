536b691bb202ad50be5bed76838f1a4e54f104d8
Thirapi <thirapi@proton.me>
2026-02-14 03:15:20 +0700

    feat: Add `is_spoiler` column to `threads` and `replies` tables and remove migration `0010`.

--- lib/supabase/migrations/0009_rapid_harry_osborn.sql
+++ lib/supabase/migrations/0009_rapid_harry_osborn.sql
@@ -7,6 +7,8 @@ CREATE TABLE "bans" (
 );
 --> statement-breakpoint
 ALTER TABLE "threads" ADD COLUMN "is_nsfw" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "threads" ADD COLUMN "is_spoiler" boolean DEFAULT false;--> statement-breakpoint
 ALTER TABLE "replies" ADD COLUMN "is_nsfw" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "replies" ADD COLUMN "is_spoiler" boolean DEFAULT false;--> statement-breakpoint
 CREATE INDEX "idx_bans_ip" ON "bans" USING btree ("ip_address");--> statement-breakpoint
 CREATE INDEX "idx_bans_expires_at" ON "bans" USING btree ("expires_at");
--- lib/supabase/migrations/0010_clammy_carnage.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "threads" ADD COLUMN "is_spoiler" boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE "replies" ADD COLUMN "is_spoiler" boolean DEFAULT false;
