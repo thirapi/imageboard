660ae304a351cb5dda8d46667dc491e8d5e672a2
Thirapi <thirapi@proton.me>
2026-03-01 17:57:35 +0700

    feat: Integrate Sentry for comprehensive error monitoring and performance tracing across client, server, and edge environments.

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://af98a07ce5dab0749a4eb532dcd76585@o4510968784879616.ingest.us.sentry.io/4510968804999169",
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
