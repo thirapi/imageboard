6d05eece6f4a079aa7369b3328d8bd57275ce0b4
Thirapi <thirapi@proton.me>
2026-03-17 15:55:43 +0700

    feat: Integrate OpenNext.js with Cloudflare Workers and Hyperdrive for deployment.

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,12 +1,32 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from "./schema"
+import * as schema from "./schema";
+import { getCloudflareContext } from '@opennextjs/cloudflare';
+import { cache } from 'react';
 
-const connectionString = process.env.DATABASE_URL;
+export const getDb = cache(() => {
+  const { env } = getCloudflareContext();
 
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set in environment variables');
-}
+  const connectionString = env.HYPERDRIVE.connectionString;
 
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema })
+  if (!connectionString) {
+    throw new Error('HYPERDRIVE binding tidak ditemukan. Pastikan sudah di-bind di wrangler.jsonc');
+  }
+
+  const client = postgres(connectionString, {
+    max: 1,
+    fetch_types: false,
+    prepare: false,
+    idle_timeout: 10,
+    connect_timeout: 5,
+  });
+
+  return drizzle(client, { schema });
+});
+
+export const db = new Proxy({} as any, {
+  get: (_, prop) => {
+    const database = getDb();
+    return (database as any)[prop];
+  },
+}) as ReturnType<typeof getDb>;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 import { withSentryConfig } from "@sentry/nextjs";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+initOpenNextCloudflareForDev();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
